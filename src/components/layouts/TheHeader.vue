9643781b23331107ba5c4088121d8efb0a1babfa
Donasun <moden138@126.com>
2019-09-09 20:10:37 +0800

    add navlist

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -1,12 +1,26 @@
-<!-- 单文件组件的文件名，应该始终以单词大写开头（PascalCase），或者始终以横线（kebab-case）连接。推荐使用大写开头的方式。 -->
 <template>
   <div class="navbar navbar-default topnav">
     <div class="container">
       <div class="navbar-header">
+        <button type="button" class="navbar-toggle" @click="toggleNav">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+
         <a href="/" class="navbar-brand">
           <span class="title">{{ logo.title }}</span>
           <img :src="logo.src" :alt="logo.title">
         </a>
+      </div>
+
+      <div id="top-navbar-collapse" :class="['collapse', 'navbar-collapse', { in: showCollapsedNav }]">
+        <ul class="nav navbar-nav">
+          <li v-for="(item, index) in navList" :class="{ active: index === activeNavIndex }">
+            <a href="#" @click="changeNavIndex(index)">{{ item }}</a>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -20,11 +34,24 @@ export default {
       logo: {
         src: `${this.uploadsUrl}communities/hIZjRRdF8oVYZy69XJnT.png`,
         title: 'VuejsCaff'
-      }
+      },
+      navList: ['社区', '头条', '问答', '教程'],
+      activeNavIndex: 0,
+      // 使用数组语法对 class 进行绑定
+      // 只有当 showCollapsedNav 返回 true 时才添加 'in' 这个类名
+      showCollapsedNav: false
     }
   },
   beforeCreate() {
     this.uploadsUrl = 'https://cdn.learnku.com/uploads/'
+  },
+  methods: {
+    changeNavIndex(index) {
+      this.activeNavIndex = index
+    },
+    toggleNav() {
+      this.showCollapsedNav = !this.showCollapsedNav
+    }
   }
 };
 </script>
