ecf08fd37aad546e7122fec78a4eaa041c42efff
Donasun <moden138@126.com>
2019-09-09 19:55:02 +0800

    add logo style

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,34 @@
+<!-- 单文件组件的文件名，应该始终以单词大写开头（PascalCase），或者始终以横线（kebab-case）连接。推荐使用大写开头的方式。 -->
+<template>
+  <div class="navbar navbar-default topnav">
+    <div class="container">
+      <div class="navbar-header">
+        <a href="/" class="navbar-brand">
+          <span class="title">{{ logo.title }}</span>
+          <img :src="logo.src" :alt="logo.title">
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+  data() {
+    return {
+      logo: {
+        src: `${this.uploadsUrl}communities/hIZjRRdF8oVYZy69XJnT.png`,
+        title: 'VuejsCaff'
+      }
+    }
+  },
+  beforeCreate() {
+    this.uploadsUrl = 'https://cdn.learnku.com/uploads/'
+  }
+};
+</script>
+
+<style scoped>
+.title { display: none;}
+</style>
