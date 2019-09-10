5fb7a1cfd7f9240dbd68fceb219a486924fd57a6
Donasun <moden138@126.com>
2019-09-10 14:43:00 +0800

    finish user

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,8 @@ const router = new Router({
     // mode：路由模式，默认值 'hash' 使用井号（ # ）作路由，
     // 'history' 可利用 History API 来完成页面跳转且无须重新加载
     mode: 'history',
+    // 当前精确激活的 <router-link>，其默认值是 'router-link-exact-active'
+    linkExactActiveClass: 'active',
     routes
 })
 // 全局前置守卫
@@ -15,9 +17,14 @@ router.beforeEach((to, from, next) => {
     // 获取仓库里的登录信息
     // 使用 router.app 可以获取 router 对应的 Vue 根实例
     // 使用实例的 $options.store 可以从选项中访问仓库
-    const auth = router.app.$options.store.state.auth
+    const app = router.app
+    const store = app.$options.store
+    const auth = store.state.auth
 
-    if (auth && to.path.indexOf('/auth/') !== -1) {
+    app.$message.hide()
+
+    if ((auth && to.path.indexOf('/auth/') !== -1) ||
+        (!auth && to.meta.auth)) {
         // 如果当前用户已登录，且目标路由包含 /auth/ ，就跳转到首页
         next('/')
     } else {
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,21 +1,46 @@
-export default [
-  {
-    path: '/auth/register',
-    name: 'Register',
-    component: () => import('@/views/auth/Register')
-  },
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import('@/views/Home')
-  },
-  {
-    path: '*',
-    redirect: '/'
-  },
-  {
-    path: '/auth/login',
-    name: 'Login',
-    component: () => import('@/views/auth/Login')
-  },
+export default [{
+        path: '/auth/register',
+        name: 'Register',
+        component: () => import('@/views/auth/Register')
+    },
+    {
+        path: '/',
+        name: 'Home',
+        component: () => import('@/views/Home')
+    },
+    {
+        path: '*',
+        redirect: '/'
+    },
+    {
+        path: '/auth/login',
+        name: 'Login',
+        component: () => import('@/views/auth/Login')
+    },
+    // 编辑资料路由
+    {
+        path: '/users/1/edit',
+        component: () => import('@/views/users/Edit'),
+        children: [{
+                // 子路由的 path 为空值 ''，表示该页面作为默认子路由
+                // 在导航到父级路由（/users/1/edit）时，就开始加载
+                path: '',
+                name: 'EditProfile',
+                component: () => import('@/views/users/Profile'),
+                meta: { auth: true }
+            },
+            {
+                path: '/users/1/edit_avatar',
+                name: 'EditAvatar',
+                component: () => import('@/views/users/Avatar'),
+                meta: { auth: true }
+            },
+            {
+                path: '/users/1/edit_password',
+                name: 'EditPassword',
+                component: () => import('@/views/users/Password.vue'),
+                meta: { auth: true }
+            }
+        ]
+    },
 ]
